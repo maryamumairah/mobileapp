b2b2b9479693345c8cb0f6057f0431275f43cda8
farihahadaina <fariha.hadaina@live.iium.edu.my>
2023-11-16 17:21:53 +0800

    form created without time

--- course_schedule/web/main.dart
+++ course_schedule/web/main.dart
@@ -1,5 +1,18 @@
 import 'dart:html';
+import 'course.dart';
 
-void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+// create list to store the courseCode, day1, time1, day2, time2
+List<Course> courseList = [];
+
+void main(){
+  // connect to the HTML elements using querySelector method 
+  InputElement courseCodeInput = querySelector('#courseCode') as InputElement;
+  SelectElement day1Input = querySelector('#day1') as SelectElement;
+  SelectElement time1Input = querySelector('#time1') as SelectElement;
+  SelectElement day2Input = querySelector('#day2') as SelectElement;
+  SelectElement time2Input = querySelector('#time2') as SelectElement;
+  ButtonElement addButton = querySelector('#addCourse') as ButtonElement;
+
+
+
 }

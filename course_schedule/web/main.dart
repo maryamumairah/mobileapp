48bf34511b1730275bc8861d0ce082e69d7e675a
maryamumairah <maryamumairah12@gmail.com>
2023-11-21 07:31:05 +0800

    viewSchedule done

--- course_schedule/web/main.dart
+++ course_schedule/web/main.dart
@@ -1,10 +1,17 @@
 import 'dart:html';
 import 'course.dart';
+import 'viewCourse/view_schedule.dart' as course_lib;
+import 'dart:convert';
+
 
 // create list to store the courseCode, day1, time1, day2, time2
-List<Course> courseList = [];
-
 void main(){
+  List<course_lib.Course> weeklySchedule = [
+    course_lib.Course('Math101', 'Monday', '8.30-9.50', 'Wednesday', '8.30-9.50'),
+    course_lib.Course('Physics202', 'Tuesday', '10.00-11.20', 'Thursday', '10.00-11.20'),
+  ];
+
+
   // connect to the HTML elements using querySelector method 
   InputElement courseCodeInput = querySelector('#courseCode') as InputElement;
   SelectElement day1Input = querySelector('#day1') as SelectElement;
@@ -15,7 +22,17 @@ void main(){
   ButtonElement viewScheduleButton = querySelector('#viewSchedule') as ButtonElement;
 
   viewScheduleButton.onClick.listen((MouseEvent event){
-    window.location.href = 'viewCourse/viewCourse.html';
+    // Convert the list to a list of maps
+  List<Map<String, String>> scheduleAsMaps = weeklySchedule.map((course) => course.toMap()).toList();
+
+  // Encode the list of maps
+  String encodedSchedule = jsonEncode(scheduleAsMaps);
+
+  // Construct the URL with the encoded list as a query parameter
+  String url = 'viewCourse/viewCourse.html?schedule=$encodedSchedule';
+
+  // Navigate to the second page
+  window.location.href = url;
   });
 
 }
--- course_schedule/web/viewCourse/view_schedule.dart
+++ course_schedule/web/viewCourse/view_schedule.dart
@@ -1,28 +1,71 @@
 import 'dart:html';
+import 'dart:convert';
 
 class Course {
   String courseCode;
-  String day;
-  String time;
+  String day1;
+  String time1;
+  String day2;
+  String time2;
 
-  Course(this.courseCode, this.day, this.time);
+  Course(this.courseCode, this.day1, this.time1, this.day2, this.time2);
+
+  Map<String, String> toMap() {
+    return {
+      'courseCode': courseCode,
+      'day1': day1,
+      'time1': time1,
+      'day2': day2,
+      'time2': time2,
+    };
+  }
+
+  factory Course.fromMap(Map<String, String> map) {
+    return Course(
+      map['courseCode'] ?? '',
+      map['day1'] ?? '',
+      map['time1'] ?? '',
+      map['day2'] ?? '',
+      map['time2'] ?? '',
+    );
+  }
 }
 
 void main() {
-  List<Course> weeklySchedule = [
-    Course('Math101', 'Monday', '8.30-9.50'),
-    Course('Physics202', 'Tuesday', '8.30-9.50'),
-  ];
+  Map<String, String> queryParams = Uri.splitQueryString(window.location.href);
+ 
+  // Retrieve the encoded list from the query parameters
+  String? encodedSchedule = queryParams['http://127.0.0.1:8080/viewCourse/viewCourse.html?schedule'];
+
+  // Decode the list
+  List<dynamic> scheduleAsMaps = jsonDecode(encodedSchedule!);
+
+  // Convert the list of maps back to a list of Course objects
+  List<Course> weeklySchedule = scheduleAsMaps
+      .map((map) => Course(
+            map['courseCode'] ?? '',
+            map['day1'] ?? '',
+            map['time1'] ?? '',
+            map['day2'] ?? '',
+            map['time2'] ?? '',
+          ))
+      .toList();
 
   updateScheduleTable(weeklySchedule);
 }
 
 void updateScheduleTable(List<Course> weeklySchedule) {
-  weeklySchedule.forEach((course){
-    String cellId = '${course.day.toLowerCase()}-${course.time.replaceAll('.', '\\.')}';
-    
-    TableCellElement dateCell = querySelector('#$cellId') as TableCellElement;
-    
-    dateCell.innerText = course.courseCode;
+  weeklySchedule.forEach((course) {
+    print(course);
+    String cellId1 =
+        '${course.day1.toLowerCase()}-${course.time1.replaceAll('.', '\\.')}';
+    String cellId2 =
+        '${course.day2.toLowerCase()}-${course.time2.replaceAll('.', '\\.')}';
+
+    TableCellElement dateCell1 = querySelector('#$cellId1') as TableCellElement;
+    TableCellElement dateCell2 = querySelector('#$cellId2') as TableCellElement;
+
+    dateCell1.innerText = course.courseCode;
+    dateCell2.innerText = course.courseCode;
   });
 }

a3b92c5c9972fef5fc14d7b0e16d887869a2627d
maryamumairah <maryamumairah12@gmail.com>
2023-11-20 17:23:20 +0800

    updated main branch

--- course_schedule/web/main.dart
+++ course_schedule/web/main.dart
@@ -6,36 +6,41 @@ import 'editCourse.dart';
 List<Map<String, String>> courseList = [];
 
 void main() {
-  // connect to the HTML elements using querySelector method
+   // connect to the HTML elements using querySelector method 
   InputElement courseCodeInput = querySelector('#courseCode') as InputElement;
-  SelectElement day1Input = querySelector('#day1') as SelectElement;
-  SelectElement time1Input = querySelector('#time1') as SelectElement;
+  SelectElement day1Input = querySelector('#day1') as SelectElement; 
+  InputElement time1Input = querySelector('#time1') as InputElement;
   SelectElement day2Input = querySelector('#day2') as SelectElement;
-  SelectElement time2Input = querySelector('#time2') as SelectElement;
+  InputElement time2Input = querySelector('#time2') as InputElement;
   ButtonElement addButton = querySelector('#addCourse') as ButtonElement;
   ButtonElement editButton = querySelector('#editCourse') as ButtonElement;
 
-  editButton.onClick.listen((event){    
-    String courseCode = courseCodeInput.value!;
-    String day1 = day1Input.selectedOptions[0].value;
-    String time1 = time1Input.selectedOptions[0].value;
-    String day2 = day2Input.selectedOptions[0].value;
-    String time2 = time2Input.selectedOptions[0].value;
-
-    courseList = editCourse(courseList, courseCode, day1, time1, day2, time2); 
-  });
-  
   // Event listeners for buttons in the HTML file
   addButton.onClick.listen((event) {
-    print('Button clicked');
+    print('addCourse button clicked');
 
     String courseCode = courseCodeInput.value!;
     String day1 = day1Input.selectedOptions[0].value;
-    String time1 = time1Input.selectedOptions[0].value;
+    String time1 = time1Input.value!;
     String day2 = day2Input.selectedOptions[0].value;
-    String time2 = time2Input.selectedOptions[0].value;
+    String time2 = time2Input.value!;
 
     courseList = addCourse(courseCode, day1, time1, day2, time2);
+
+  });
+  
+  editButton.onClick.listen((event){
+    print('editCourse button clicked');
+    
+    String courseCode = courseCodeInput.value!;
+    String day1 = day1Input.selectedOptions[0].value;
+    String time1 = time1Input.value!;
+    String day2 = day2Input.selectedOptions[0].value;
+    String time2 = time2Input.value!;
+
+    courseList = editCourse(courseList, courseCode, day1, time1, day2, time2);
+    
+    print('in main.dart, courseList: $courseList');
   });
 
 }

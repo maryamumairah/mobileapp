8a1248ed49a8e83b9048584cdc30bacf4099b69e
nadirah <nadirahros.liza@live.iium.edu.my>
2023-12-04 20:29:18 +0800

    Fix error handling in addcourse

--- course_schedule/web/editCourse.dart
+++ course_schedule/web/editCourse.dart
@@ -1,12 +1,13 @@
 import 'dart:html';
 import 'overlap.dart';
 
-
 List<Map<String, String>> editCourse(List<Map<String, String>> courseList, String courseCode, String day1, String time1, String day2, String time2) {
-
-  Map<String, String> newCourse = { //this creates a new map called newCourse with the following keys and values but it is not needed because we already have newCourse in the function parameters
-    'courseCode': courseCode, //this line is not needed because we already have courseCode in the function parameters
-    'day1': day1, //this line is to add the day1 to the newCourse map but it is not needed because we already have day1 in the function parameters
+  Map<String, String> newCourse = {
+    //this creates a new map called newCourse with the following keys and values but it is not needed because we already have newCourse in the function parameters
+    'courseCode':
+        courseCode, //this line is not needed because we already have courseCode in the function parameters
+    'day1':
+        day1, //this line is to add the day1 to the newCourse map but it is not needed because we already have day1 in the function parameters
     'time1': time1,
     'day2': day2,
     'time2': time2,
@@ -17,32 +18,30 @@ List<Map<String, String>> editCourse(List<Map<String, String>> courseList, Strin
 
   DivElement messageDiv = querySelector('#message') as DivElement;
   if (courseExists) {
-      // Check if the updated course has an overlap with any existing course
-      if (!hasOverlap(courseList, newCourse, courseCode, day1, time1, day2, time2)) { 
-        // If there's no overlap, update the course
-        
-        for(Map<String, String> courseListTemp in courseList) {
-          if(courseListTemp['courseCode'] == courseCode) { 
-            courseListTemp['day1'] = day1; 
-            courseListTemp['time1'] = time1;
-            courseListTemp['day2'] = day2;
-            courseListTemp['time2'] = time2;
+    // Check if the updated course has an overlap with any existing course
+    if (!hasOverlap(courseList, newCourse, day1, time1, day2, time2)) {
+      // If there's no overlap, update the course
 
-            courseListTemp = newCourse;
+      for (Map<String, String> courseListTemp in courseList) {
+        if (courseListTemp['courseCode'] == courseCode) {
+          courseListTemp['day1'] = day1;
+          courseListTemp['time1'] = time1;
+          courseListTemp['day2'] = day2;
+          courseListTemp['time2'] = time2;
+          courseListTemp = newCourse;
 
-            break;
-          }
+          break;
         }
-
-        messageDiv.text = 'Course updated successfully.';
-      } else {
-        // If there's an overlap, print an error message
-        messageDiv.text = 'Error: The updated course overlaps with an existing course. Please check the course times again.';
       }
+
+      messageDiv.text = 'Course updated successfully.';
+    } else {
+      // If there's an overlap, print an error message
+      messageDiv.text = 'Error: The updated course overlaps with an existing course. Please check the course times again.';
+    }
   } else {
     messageDiv.text = 'Error: Course $courseCode does not exist in the schedule. Please check the course code again.';
   }
 
   return courseList;
-
 }
--- course_schedule/web/overlap.dart
+++ course_schedule/web/overlap.dart
@@ -1,6 +1,7 @@
-bool hasOverlap(List<Map<String, String>> courseList, Map<String, String> newCourse, String courseCode, String day1, String time1, String day2, String time2) {
+bool hasOverlap(List<Map<String, String>> courseList, Map<String, String> newCourse, String day1, String time1, String day2, String time2) {
   for (Map<String, String> existingCourse in courseList) {
-    if (existingCourse['day1'] == newCourse['day1'] ||
+    if (existingCourse['courseCode'] == newCourse['courseCode']) {
+      if (existingCourse['day1'] == newCourse['day1'] ||
         existingCourse['day1'] == newCourse['day2'] ||
         existingCourse['day2'] == newCourse['day1'] ||
         existingCourse['day2'] == newCourse['day2']) {
@@ -8,6 +9,25 @@ bool hasOverlap(List<Map<String, String>> courseList, Map<String, String> newCou
           existingCourse['time1'] == newCourse['time2'] ||
           existingCourse['time2'] == newCourse['time1'] ||
           existingCourse['time2'] == newCourse['time2']) {
+        return true; // Overlapping day and time
+      }
+    }
+    }
+  }
+  return false;
+}
+
+
+bool hasOverlapWithExisting(List<Map<String, String>> courseList, String courseCode, String day1, String time1, String day2, String time2) {
+  for (Map<String, String> existingCourse in courseList) {
+    if (existingCourse['day1'] == day1 ||
+        existingCourse['day1'] == day2 ||
+        existingCourse['day2'] == day1 ||
+        existingCourse['day2'] == day2) {
+      if (existingCourse['time1'] == time1 ||
+          existingCourse['time1'] == time2 ||
+          existingCourse['time2'] == time1 ||
+          existingCourse['time2'] == time2) {
         return true; // Overlapping day and time
       }
     }

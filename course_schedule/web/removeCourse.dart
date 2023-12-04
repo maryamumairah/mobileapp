07c581e0971a1702f1f60be7f739c59534117240
maryamumairah <maryamumairah12@gmail.com>
2023-12-04 22:33:15 +0800

    Revert "Merge pull request #44 from maryamumairah/41-change-message-style"
    This reverts commit 086ad1fc30419fd997c0046269a80052f69d25a5, reversing
    changes made to 798604f894ff3fee9cf84e7570cc6881af61ae73.

--- course_schedule/web/removeCourse.dart
+++ course_schedule/web/removeCourse.dart
@@ -1,20 +1,14 @@
 import 'dart:html';
 
-List<Map<String, String>> removeCourse(
-    List<Map<String, String>> courseList, String course2Remove) {
-  bool courseExist =
-      courseList.any((course) => course['courseCode'] == course2Remove);
+List<Map<String, String>> removeCourse(List<Map<String, String>> courseList, String course2Remove){
+  bool courseExist = courseList.any((course) => course['courseCode'] == course2Remove);
 
   if (courseExist) {
     courseList.removeWhere((course) => course['courseCode'] == course2Remove);
 
-    document.getElementById('message')!.innerText =
-        'Course $course2Remove removed from schedule';
-    document.getElementById('message')!.style.color = 'green';
+    document.getElementById('alertR')!.innerText = 'Course $course2Remove removed';
   } else {
-    document.getElementById('message')!.innerText =
-        'Error: Course $course2Remove not found in schedule';
-    document.getElementById('message')!.style.color = 'red';
+    document.getElementById('alertR')!.innerText = 'Course $course2Remove not found';
   }
   return courseList;
 }

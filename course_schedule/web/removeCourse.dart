f9fcccb5b8c41fefaec9426553384311f60b2b4f
maryamumairah <maryamumairah12@gmail.com>
2023-12-04 23:06:46 +0800

    fix error handling in editcourse,removecourse,viewschedule

--- course_schedule/web/removeCourse.dart
+++ course_schedule/web/removeCourse.dart
@@ -1,19 +1,15 @@
 import 'dart:html';
 
-List<Map<String, String>> removeCourse(
-    List<Map<String, String>> courseList, String course2Remove) {
-  bool courseExist =
-      courseList.any((course) => course['courseCode'] == course2Remove);
+List<Map<String, String>> removeCourse(List<Map<String, String>> courseList, String course2Remove) {
+  bool courseExist = courseList.any((course) => course['courseCode'] == course2Remove);
 
   if (courseExist) {
     courseList.removeWhere((course) => course['courseCode'] == course2Remove);
 
-    document.getElementById('message')!.innerText =
-        'Course $course2Remove removed from schedule';
+    document.getElementById('message')!.innerText = 'Course $course2Remove removed from schedule';
     document.getElementById('message')!.style.color = 'green';
   } else {
-    document.getElementById('message')!.innerText =
-        'Error: Course $course2Remove not found in schedule';
+    document.getElementById('message')!.innerText = 'Error: Course $course2Remove not found in schedule';
     document.getElementById('message')!.style.color = 'red';
   }
   return courseList;
